2dbbdbb8cb53e68a6c2c8c694aa9cc1eb6e50876
avibenita <39944593+avibenita@users.noreply.github.com>
2026-03-15 11:50:57 +0200

    Update regression-input-panel.js

--- statistico-analytics/taskpane/regression/regression-input-panel.js
+++ statistico-analytics/taskpane/regression/regression-input-panel.js
@@ -65,7 +65,7 @@ function openModelBuilder() {
 
   Office.context.ui.displayDialogAsync(
     dialogUrl,
-    { height: 88, width: 72, displayInIframe: false },
+    { height: 88, width: 25, displayInIframe: false },
     (asyncResult) => {
       if (asyncResult.status === Office.AsyncResultStatus.Failed) {
         console.error('Failed to open dialog:', asyncResult.error);
